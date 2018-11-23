dc4675dbfc70f3fabd2aa8e0bc8aea5a3b145b21
Robson Rogerio Silva <robson.rogerio@poupex.com.br>
2018-11-23 17:30:05 -0200

    Ajuste de extensão para não gerar problema.

--- test/validaAdvpl.js
+++ test/validaAdvpl.js
@@ -1,12 +1,12 @@
 const fileSystem = require("file-system");
 let validaAdvpl = require("../lib/validaAdvpl");
 let directoryPath = __dirname + '\\files';
-let conteudo = fileSystem.readFileSync(directoryPath + "\\COM ERRO.prw", "latin1");
+let conteudo = fileSystem.readFileSync(directoryPath + "\\COM ERRO", "latin1");
 
 let objeto = new validaAdvpl.ValidaAdvpl([]);
 //seta variáveis 
 objeto.ownerDb = ['PROTHEUS'];
 objeto.empresas= ['01'];
 
-objeto.validacao(conteudo,"COM ERRO.prw");
+objeto.validacao(conteudo,"COM ERRO");
 console.log(objeto.error);
